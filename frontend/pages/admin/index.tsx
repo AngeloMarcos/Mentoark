3feffc81742aac57eca14954ad220b2051601716
Angelo Marcos <64624199+AngeloMarcos@users.noreply.github.com>
2025-09-30 06:32:20 +0000

    sa

--- frontend/pages/admin/index.tsx
+++ frontend/pages/admin/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Head from 'next/head';
-import { Header } from '../components/Header';
-import { Hero } from '../components/Hero';
-import { Features } from '../components/Features';
-import { HowItWorks } from '../components/HowItWorks';
-import { Integrations } from '../components/Integrations';
-import { Testimonials } from '../components/Testimonials';
-import { CTA } from '../components/CTA';
-import { Footer } from '../components/Footer';
+import React from 'react';
+import Head from 'next/head';
+
+import Header from '../../components/Header';
+import Hero from '../../components/Hero';
+import Features from '../../components/Features';
+import HowItWorks from '../../components/HowItWorks';
+import { Integrations } from '../../components/Integrations';
+import { Testimonials } from '../../components/Testimonials';
+import { CTA } from '../../components/CTA';
+import { Footer } from '../../components/Footer';
 
 export default function Home() {
   return (
